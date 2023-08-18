719dad0632a6ff8ec1aa7321ab9f4aed06445049
Fabian-Frenzel <fabian.frenzel@stud.hs21.de>
2023-08-18 16:20:57 +0200

    Updated: - README.md - create first menu - btnMechanic Click Event created

--- app/src/main/java/com/example/wikiapp/MainActivity.kt
+++ app/src/main/java/com/example/wikiapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.wikiapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var btnMechanic = findViewById<Button>(R.id.btnMechanic)
+        btnMechanic.setOnClickListener {
+
+        }
     }
+
 }
