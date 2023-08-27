7225a16fcbc38aac9f6cfafe4bffd15ea5b9e523
Niko-Ri <rifertniko@gmail.com>
2023-08-27 21:42:49 +0200

    First Setup done

--- app/src/main/java/com/example/wikiapp/MainActivity.kt
+++ app/src/main/java/com/example/wikiapp/MainActivity.kt
@@ -4,18 +4,35 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.example.wikiapp.mainmenu.ComputerScienceMenu
+import com.example.wikiapp.mainmenu.MathMenu
+import com.example.wikiapp.mainmenu.PhysicsMenu
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var btnMechanic = findViewById<Button>(R.id.btn_mechanic)
-        btnMechanic.setOnClickListener {
-            val intent = Intent(this, MechanicMenu::class.java)
+
+        var btnComputerScience = findViewById<Button>(R.id.btnComputerScience)
+        btnComputerScience.setOnClickListener {
+            val intent = Intent(this, ComputerScienceMenu::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        var btnMath = findViewById<Button>(R.id.btnMath)
+        btnMath.setOnClickListener {
+            val intent = Intent(this, MathMenu::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        var btnPhysics = findViewById<Button>(R.id.btnPhysics)
+        btnPhysics.setOnClickListener {
+            val intent = Intent(this, PhysicsMenu::class.java)
             startActivity(intent)
             finish()
         }
     }
-
 }
